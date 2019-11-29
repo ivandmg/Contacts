4a145701ea5249a4b411d989a8cad232dc40b670
ivandmg150@gmail.com <visual_community@outlook.com>
2019-11-29 16:45:54 -0400

    commit hasta tokernresponse

---
+++ apiContacts/AppContact/AppContact/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace AppContact.Models
+{
+    public class Contacts
+    {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Phone { get; set; }
+
+        public string Bussines { get; set; }
+
+    }
+}
---
+++ apiContacts/AppContact/AppContact/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppContact.Models
+{
+    public class response
+    {
+        public bool ISSuccess { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+
+    }
+}
